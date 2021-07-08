06ccf57868acaf6ffe924b45231cb1cc1e4f928c
isabeli-fernandes <isabelidsf@outlook.com>
2021-07-08 15:32:06 -0300

    exercicio-01

---
+++ 01-exercicio.rb
@@ -0,0 +1,14 @@
+puts "Quantas horas há em um ano?"
+puts 24 * 365 # Hora por dia*1 Ano"
+
+puts "Quantos minutos há em uma década?"
+puts 1440*3650 # Minutos em um dia*Dias em uma década
+
+puts "Qual é a sua idade em segundos?"
+puts 31536000*20 # Segundos em um ano*Idade
+
+puts "Quantos chocolates você pretende comer na vida?"
+puts 84*30*1 # Idade de vida*dias*1 por dia 
+
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts 1232000000/31536000 #1232 milhões de segundos/Nºde segundos no ano 

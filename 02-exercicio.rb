3b688bde92f4925814b31b345e7edf3cdfed055f
isabeli-fernandes <isabelidsf@outlook.com>
2021-07-08 17:15:52 -0300

    exercicio2

---
+++ 02-exercicio.rb
@@ -0,0 +1,55 @@
+# Quantas horas há em um ano?
+
+hora = 24
+ano = 365
+
+horas = hora*ano
+
+puts "Quantas horas há em um ano?"
+puts horas
+
+#Quantos minutos há em uma década?
+
+horas = 24
+minutos = 60
+ano = 365
+decada = 10
+
+minutosDecada = horas*minutos*ano*decada
+puts "Quantos minutos há em uma década?"
+puts minutosDecada
+
+#Qual é a sua idade em segundos?
+
+ano = 365
+horas = 24
+minutos = 60
+segundos = 60
+idade = 20
+
+idadeSegundos = ano*horas*minutos*segundos*idade
+puts "Qual é a sua idade em segundos?"
+puts idadeSegundos
+
+#Quantos chocolates você pretende comer na vida?
+
+anos = 84
+dias = 30
+chocolateDia = 1
+
+chocolateVida = anos*dias*chocolateDia
+puts "Quantos chocolates você pretende comer na vida?"
+puts chocolateVida
+
+#Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?
+
+idade = 1232000000
+ano = 365
+horas = 24
+minutos = 60
+segundos = 60
+
+idadeAnos = idade / ano / horas / minutos / segundos
+
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts idadeAnos

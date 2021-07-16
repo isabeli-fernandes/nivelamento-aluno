231f9b620ae9c304df4773e3d275832a25df6929
isabeli-fernandes <isabelidsf@outlook.com>
2021-07-16 09:48:18 -0300

    Exercicio08-parte02

---
+++ 08-exercicio-parte-2.rb
@@ -0,0 +1,29 @@
+#Faça um programa para calcular o Mínimo Múltiplo Comum (MMC) entre dois números inteiros.
+#Como o próprio nome diz, o MMC é o menor número inteiro que é múltiplo comum de outros números.
+#Perguntar ao usuários os dois números inteiros que ele quer saber o MMC.
+
+print "Por favor, digite um número: "
+a = gets.to_i
+
+print "Por favor, digite outro número: "
+b = gets.to_i
+resto = resto != 0
+
+def mmc (a,b)
+    resto = a % b
+    a = b
+    b = resto
+    return
+end
+    while resto 
+    resultado = ( a * b) 
+    print " O MMC dos números digitados é:" + resultado.to_s
+    return
+end
+
+
+
+   
+
+
+

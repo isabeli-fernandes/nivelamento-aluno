6c0c1df40288f1ab2b2828eda94117377f0c30b1
isabeli-fernandes <isabelidsf@outlook.com>
2021-07-18 17:58:05 -0300

    Exercicio10-pt02

---
+++ 10-exercicio-parte-2.rb
@@ -0,0 +1,71 @@
+#Esse aqui é um desafio. Faça um código para obter esse resultado:
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+# *******
+# ********
+# *********
+# **********
+for x in (1..1) #linha
+    for y in (1..1) #coluna
+        print "*"
+    end
+    puts
+end
+for x in (2..2) 
+    for y in (1..2) 
+        print "*"
+    end
+    puts
+end
+for x in (3..3) 
+    for y in (1..3) 
+        print "*"
+    end
+    puts
+end
+for x in (4..4) 
+    for y in (1..4) 
+        print "*"
+    end
+    puts
+end
+for x in (5..5) 
+    for y in (1..5) 
+        print "*"
+    end
+    puts
+end
+for x in (6..6) 
+    for y in (1..6) 
+        print "*"
+    end
+    puts
+end
+for x in (7..7) 
+    for y in (1..7) 
+        print "*"
+    end
+    puts
+end
+for x in (8..8) 
+    for y in (1..8) 
+        print "*"
+    end
+    puts
+end
+for x in (9..9) 
+    for y in (1..9) 
+        print "*"
+    end
+    puts
+end
+for x in (10..10) 
+    for y in (1..10) 
+        print "*"
+    end
+    puts
+end
